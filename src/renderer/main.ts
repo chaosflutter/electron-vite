d678dc6e6fa9b10a68dea9f487c2a7ae60b0011b
chaosflutter <ivzhangyan@gmail.com>
2023-09-14 16:17:33 +0800

    chore: update

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -6,17 +6,4 @@ import App from './App.vue'
 import { router } from './router'
 import { createPinia } from 'pinia'
 
-import { db } from '../common/db'
-
-console.log(db)
-
 createApp(App).use(createPinia()).use(router).mount('#app')
-
-// db('Chat')
-//   .first()
-//   .then(obj => {
-//     console.log(obj)
-//   })
-//   .catch(err => {
-//     console.log(err)
-//   })
